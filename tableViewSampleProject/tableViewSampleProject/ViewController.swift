5e8cb05c96b5eb3e8bf48cf7bb050d84d6db5cb7
YamamotoKazunori <reform.kazunori@gmail.com>
2017-05-03 18:46:04 +0900

    add: gitignore file.

---
+++ tableViewSampleProject/tableViewSampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableViewSampleProject
+//
+//  Created by { Kazunori } on 2017/05/03.
+//  Copyright © 2017年 Yamamoto Kazunori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
