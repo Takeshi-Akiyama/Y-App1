31a9f6eb15ef1bb6b4849618832a09b6e05a713e
YamamotoKazunori <reform.kazunori@gmail.com>
2017-05-04 08:58:07 +0900

    add: lifeCycle method.

--- tableViewSampleProject/tableViewSampleProject/ViewController.swift
+++ tableViewSampleProject/tableViewSampleProject/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
